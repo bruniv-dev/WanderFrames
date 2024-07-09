b552aeb0110798e67146d5e0d159e57651fca37e
= <bruniv.dev@gmail.com>
2024-07-09 19:51:21 +0530

    homepageUpdate

--- frontend/src/components/Home/home.js
+++ frontend/src/components/Home/home.js
@@ -6,8 +6,16 @@ import slides from "../data/caraouselData.json";
 
 const Home = () => {
   return (
-    <div className="slider-container">
+    <div className="container">
+      <h1 className="heading">WANDER FRAMES</h1>
+      <h4 className="sub-heading">
+        From your lens to the world: let your adventures inspire.
+      </h4>
       <Carousel data={slides.slides} />
+      <div className="hero-buttons">
+        <button className="hero-button-1">Get Inspired</button>
+        <button className="hero-button-2">Share Your Journey</button>
+      </div>
     </div>
   );
 };
