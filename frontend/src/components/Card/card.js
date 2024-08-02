d26bebe09bdf9f7c277b54ead3dff0d5ab0343b2
= <bruniv.dev@gmail.com>
2024-08-03 00:46:09 +0530

    favs

--- frontend/src/components/Card/card.js
+++ frontend/src/components/Card/card.js
@@ -387,6 +387,163 @@
 
 // export default Card;
 
+// import React, { useState, useEffect } from "react";
+// import { MdLocationOn, MdDeleteForever, MdEdit } from "react-icons/md";
+// import {
+//   toggleFavorite,
+//   fetchUserDetailsById,
+//   deletePostById,
+// } from "../api-helpers/helpers";
+// import "./Card.css";
+// import { useNavigate } from "react-router-dom";
+
+// const Card = ({
+//   image,
+//   location,
+//   subLocation,
+//   description,
+//   date,
+//   _id,
+//   userId,
+//   locationUrl,
+//   onFavoriteToggle,
+//   onDelete,
+//   isAdmin,
+//   onAdminDelete,
+//   onCardClick,
+// }) => {
+//   const mainImageUrl = image?.url || "https://placehold.co/600x400";
+//   const [isFavorite, setIsFavorite] = useState(false);
+//   const [userDetails, setUserDetails] = useState({});
+//   const [loggedInUserId, setLoggedInUserId] = useState(null);
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+//     setIsFavorite(favorites.includes(_id));
+
+//     const storedUserId = localStorage.getItem("userId");
+//     setLoggedInUserId(storedUserId);
+
+//     if (userId) {
+//       fetchUserDetailsById(userId)
+//         .then((user) => setUserDetails(user))
+//         .catch((err) => console.error("Error fetching user details:", err));
+//     }
+//   }, [_id, userId]);
+
+//   const handleFavoriteClick = (e) => {
+//     e.stopPropagation(); // Prevent event propagation to the card container
+//     if (_id) {
+//       toggleFavorite(_id)
+//         .then(() => {
+//           const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+//           if (isFavorite) {
+//             localStorage.setItem(
+//               "favorites",
+//               JSON.stringify(favorites.filter((favId) => favId !== _id))
+//             );
+//           } else {
+//             localStorage.setItem(
+//               "favorites",
+//               JSON.stringify([...favorites, _id])
+//             );
+//           }
+//           setIsFavorite(!isFavorite);
+//           if (onFavoriteToggle) {
+//             onFavoriteToggle();
+//           }
+//         })
+//         .catch((err) => console.log("Error in toggleFavorite:", err));
+//     } else {
+//       console.error("Post ID (_id) is missing");
+//     }
+//   };
+
+//   const handleDeleteClick = (e) => {
+//     e.stopPropagation(); // Prevent event propagation to the card container
+//     if (window.confirm("Are you sure you want to delete this post?")) {
+//       deletePostById(_id)
+//         .then(() => {
+//           if (onDelete) {
+//             onDelete(_id); // Notify parent component to refresh the list
+//           }
+//         })
+//         .catch((err) => console.error("Error deleting post:", err));
+//     }
+//   };
+
+//   const handleAdminDeleteClick = (e) => {
+//     e.stopPropagation(); // Prevent event propagation to the card container
+//     if (window.confirm("Are you sure you want to delete this post?")) {
+//       if (onAdminDelete) {
+//         onAdminDelete(_id); // Notify parent component to refresh the list
+//       }
+//     }
+//   };
+
+//   const handleEditClick = (e) => {
+//     e.stopPropagation(); // Prevent event propagation to the card container
+//     navigate(`/editPost/${_id}`); // Navigate to the edit post page with the post ID
+//   };
+
+//   const handleUsernameClick = (e) => {
+//     e.stopPropagation(); // Prevent event propagation to the card container
+//     navigate(`/userProfile/${userId}`); // Navigate to the user's profile page
+//   };
+
+//   return (
+//     <div className="card-container" onClick={onCardClick}>
+//       <img className="main-image" src={mainImageUrl} alt="Main" />
+//       <div className="card-header">
+//         <img
+//           className="profile-image-card"
+//           src={userDetails.profileImage}
+//           alt="profilepic"
+//         />
+//         <div className="user-info">
+//           <p className="username" onClick={handleUsernameClick}>
+//             {userDetails.name || "Unknown User"}
+//           </p>
+//           <p className="date">{new Date(date).toLocaleDateString()}</p>
+//         </div>
+//         {locationUrl && (
+//           <a href={locationUrl} target="_blank" rel="noopener noreferrer">
+//             <MdLocationOn className="location-button" />
+//           </a>
+//         )}
+//         <button
+//           className={`add-to-favorites ${isFavorite ? "favorite" : ""}`}
+//           onClick={handleFavoriteClick}
+//         >
+//           {isFavorite ? "-" : "+"}
+//         </button>
+//         {(loggedInUserId === userId || isAdmin) && (
+//           <MdDeleteForever
+//             className="delete-button"
+//             onClick={handleDeleteClick}
+//           />
+//         )}
+//         {loggedInUserId === userId && (
+//           <MdEdit className="edit-button" onClick={handleEditClick} />
+//         )}
+//         {onAdminDelete && (
+//           <button className="delete-button" onClick={handleAdminDeleteClick}>
+//             Delete
+//           </button>
+//         )}
+//       </div>
+//       <div className="card-content">
+//         <p className="location">{location}</p>
+//         <p className="sub-location">{subLocation}</p>
+//         <p className="description">{description}</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Card;
+
 import React, { useState, useEffect } from "react";
 import { MdLocationOn, MdDeleteForever, MdEdit } from "react-icons/md";
 import {
@@ -419,11 +576,13 @@ const Card = ({
   const navigate = useNavigate();
 
   useEffect(() => {
-    const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-    setIsFavorite(favorites.includes(_id));
-
     const storedUserId = localStorage.getItem("userId");
     setLoggedInUserId(storedUserId);
+
+    // Use user-specific favorites key
+    const favoritesKey = `favorites_${storedUserId}`;
+    const favorites = JSON.parse(localStorage.getItem(favoritesKey)) || [];
+    setIsFavorite(favorites.includes(_id));
 
     if (userId) {
       fetchUserDetailsById(userId)
@@ -432,27 +591,29 @@ const Card = ({
     }
   }, [_id, userId]);
 
+  const updateFavoritesInLocalStorage = (newFavorites) => {
+    const favoritesKey = `favorites_${loggedInUserId}`;
+    localStorage.setItem(favoritesKey, JSON.stringify(newFavorites));
+  };
+
   const handleFavoriteClick = (e) => {
-    e.stopPropagation(); // Prevent event propagation to the card container
+    e.stopPropagation();
     if (_id) {
       toggleFavorite(_id)
         .then(() => {
-          const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-          if (isFavorite) {
-            localStorage.setItem(
-              "favorites",
-              JSON.stringify(favorites.filter((favId) => favId !== _id))
-            );
-          } else {
-            localStorage.setItem(
-              "favorites",
-              JSON.stringify([...favorites, _id])
-            );
-          }
-          setIsFavorite(!isFavorite);
-          if (onFavoriteToggle) {
-            onFavoriteToggle();
-          }
+          setIsFavorite((prevIsFavorite) => {
+            const favoritesKey = `favorites_${loggedInUserId}`;
+            const favorites =
+              JSON.parse(localStorage.getItem(favoritesKey)) || [];
+            const newFavorites = prevIsFavorite
+              ? favorites.filter((favId) => favId !== _id)
+              : [...favorites, _id];
+            updateFavoritesInLocalStorage(newFavorites);
+            if (onFavoriteToggle) {
+              onFavoriteToggle();
+            }
+            return !prevIsFavorite;
+          });
         })
         .catch((err) => console.log("Error in toggleFavorite:", err));
     } else {
@@ -461,12 +622,12 @@ const Card = ({
   };
 
   const handleDeleteClick = (e) => {
-    e.stopPropagation(); // Prevent event propagation to the card container
+    e.stopPropagation();
     if (window.confirm("Are you sure you want to delete this post?")) {
       deletePostById(_id)
         .then(() => {
           if (onDelete) {
-            onDelete(_id); // Notify parent component to refresh the list
+            onDelete(_id);
           }
         })
         .catch((err) => console.error("Error deleting post:", err));
@@ -474,22 +635,22 @@ const Card = ({
   };
 
   const handleAdminDeleteClick = (e) => {
-    e.stopPropagation(); // Prevent event propagation to the card container
+    e.stopPropagation();
     if (window.confirm("Are you sure you want to delete this post?")) {
       if (onAdminDelete) {
-        onAdminDelete(_id); // Notify parent component to refresh the list
+        onAdminDelete(_id);
       }
     }
   };
 
   const handleEditClick = (e) => {
-    e.stopPropagation(); // Prevent event propagation to the card container
-    navigate(`/editPost/${_id}`); // Navigate to the edit post page with the post ID
+    e.stopPropagation();
+    navigate(`/editPost/${_id}`);
   };
 
   const handleUsernameClick = (e) => {
-    e.stopPropagation(); // Prevent event propagation to the card container
-    navigate(`/userProfile/${userId}`); // Navigate to the user's profile page
+    e.stopPropagation();
+    navigate(`/userProfile/${userId}`);
   };
 
   return (
